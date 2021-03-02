9e65c88c25c03bf023e861a8106457da366f79ba
Flavio <flaviomuscente@libero.it>
2021-03-02 18:23:07 +0100

    add restaurant controller and index page

---
+++ app/Http/Controllers/RestaurantController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+/*
+
+*da definire le validazioni per il ristorante
+
+*use App\Http\Requests\RestaurantRequest;
+
+*/
+
+use App\Restaurant;
+
+use App\Foood;
+
+use Illuminate\Support\Str;
+
+use Illuminate\Support\Facades\Auth;
+
+class RestaurantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function __construct()
+    {
+        $this->middleware('auth')->except('index', 'show');
+    }
+
+    public function index()
+    {
+        $restaurants = Restaurant::all();
+
+        $userLogged = Auth::user();
+
+
+        return view('restaurants.index', compact('restaurants', 'userLogged'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
