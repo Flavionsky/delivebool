97bc9c073ab338304f1e46af59de0311ae534da8
Flavio Muscente <flaviomuscente@libero.it>
2021-03-06 20:11:56 +0100

    small fix register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -86,7 +86,7 @@ Delivebool - Crea un nuovo account
                             @csrf
 
                             <div class="form-group row">
-                                <label for="name" class="col-md-4 col-form-label text-md-right">Nome Ristorante</label>
+                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nome Ristorante') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
