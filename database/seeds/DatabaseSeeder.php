d4bb6a27ae338e3956f4f0cef0c65904ee6d59a7
Flavio Muscente <flaviomuscente@libero.it>
2021-03-15 14:58:36 +0100

    removed all faker

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(RestaurantSeeder::class);
+
         $this->call(TypeSeeder::class);
-        $this->call(FoodSeeder::class);
+
     }
 }
